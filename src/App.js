e19ef8f50e4b61787fcc6438c57ede245c4a915d
priyanshi vastani <vastanipriyanshi24@gmail.com>
2024-05-04 19:34:13 +0530

    added radius

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
             <img
               src="https://images.unsplash.com/photo-1543269664-56d93c1b41a6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Profile"
+              className="rounded-image"
             />
           </div>
           <div className="section__text">
