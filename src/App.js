0d113f659777949e3edc06606707d46079f985e5
priyanshi vastani <vastanipriyanshi24@gmail.com>
2024-05-08 16:26:51 +0530

    added dtata

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ function App() {
                 src="https://plus.unsplash.com/premium_photo-1673976275849-986056b83cae?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Profile picture"
                 className="about-pic"
+          
               />
             </div>
             <div className="about-details-container">
